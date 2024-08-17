8a1b5e8b0603d1c3597f7ac70d4dd18245e3c9fd
つづこ <>
2024-08-17 10:03:30 +0900

    記事の投稿〜保存の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'articles#index'
-  
+
   # いろんなurlをrailsが作ってくれる
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :new, :create]
+
 
 
 end
