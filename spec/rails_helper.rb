4c257472b9beff95d3d37f0f8bdd678c1a7106d9
つづこ <>
2025-04-09 01:14:19 +0900

    user作成時にprofileも作成し、プロフィールが表示できることをテスト

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,4 +64,6 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
+
 end
