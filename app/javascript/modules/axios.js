ab40021adf4e9b9f24b825e888effacaa225337d
つづこ <>
2025-07-11 21:23:53 +0900

    JavaScript読み込みについて修正

--- app/javascript/modules/axios.js
+++ app/javascript/modules/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
-import { csrfToken } from 'rails-ujs'
+// import { csrfToken } from 'rails-ujs'
+import Rails from "@rails/ujs"
 
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+axios.defaults.headers.common['X-CSRF-Token'] = Rails.csrfToken()
 
 // ↓はapplication.jsじゃなくて、このファイル？
 // import Rails from "@rails/ujs"
